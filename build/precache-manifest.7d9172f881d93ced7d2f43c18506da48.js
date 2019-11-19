68b9cfcfa9909266834bdcac9cfdf443c71116d2
dimshades <qq174028@sina.com>
2019-11-20 00:07:22 +0800

    add built files

---
+++ build/precache-manifest.7d9172f881d93ced7d2f43c18506da48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21d790de18ba4a753775319ad127847d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "541894bd6482f29d7c7e",
+    "url": "/static/css/main.ed61034b.chunk.css"
+  },
+  {
+    "revision": "d0cf89fed89eb48d8940",
+    "url": "/static/js/2.5c4523f3.chunk.js"
+  },
+  {
+    "revision": "541894bd6482f29d7c7e",
+    "url": "/static/js/main.d7ce558f.chunk.js"
+  },
+  {
+    "revision": "4721beea84fa606bc523",
+    "url": "/static/js/runtime-main.fd4df968.js"
+  }
+]);
