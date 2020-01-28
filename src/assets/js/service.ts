e5dec90dc599ff9f75ae929ac0bc2336267333c7
dimshades <qq174028@sina.com>
2020-01-28 08:52:35 +0800

    update: NOT WORKALBE, ad format

--- src/assets/js/service.ts
+++ src/assets/js/service.ts
@@ -4,24 +4,27 @@ export interface RecordData {
     [propName: string]: { timestamp: string; value: string };
 }
 
-export interface EstateDataDto {
-    id: number
-    record: RecordData
-    createAt?: string
-    updateAt?: string
-    deleteAt?: string
+interface BaseDataDto {
+    id: number;
+    record: RecordData;
+    createAt?: string;
+    updateAt?: string;
+    deleteAt?: string;
 }
+export interface EstateDataDto extends BaseDataDto {}
+export interface PriceDataDto extends BaseDataDto {}
 
 /**
  * 用于调用服务端接口
  * @export
  */
 export default class ServerApi {
-    public static serverUrl = 'http://caperal.cn:3141'
+    public static serverUrl = 'http://caperal.cn:3141';
     // public static serverUrl = 'http://localhost:3141'
 
-    public static async rawGetRequest<T = AxiosResponse<EstateDataDto[] | any>>(api: string) {
-        const result = await axios.get<T>(`${this.serverUrl}/${api}`)
-        return result
+    public static async getListData<T = AxiosResponse<BaseDataDto[] | any>>(api: string, name?: string) {
+        let destination = name ? `${api}/${name}` : `${api}`;
+        const result = await axios.get<T>(`${this.serverUrl}/${destination}`);
+        return result;
     }
 }
