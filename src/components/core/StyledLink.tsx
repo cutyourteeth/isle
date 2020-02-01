054ea947d3e99499dc284d1e1a8a1f4a6fe946dd
dimlights <qq174028@sina.com>
2020-02-01 16:50:19 +0800

    custom link style

--- src/components/core/StyledLink.tsx
+++ src/components/core/StyledLink.tsx
@@ -5,50 +5,33 @@ import styled from 'styled-components';
 
 export const StyledLink = (props: H.LocationState) => {
     const [active, setActive] = useState(false);
-        const history = useHistory()
-        const changeState = () => {
-        history.push(props.to)
-        setActive(s => !!s);
+    const history = useHistory();
+    const changeState = () => {
+        history.push(props.to);
+        setActive(s => !s);
     };
 
     return (
-        <LinkWrapper onClick={changeState} className={active ? 'active' : 'inactive'}>
+        <LinkWrapper onClick={changeState} theme={{ active }}>
             {props.children}
         </LinkWrapper>
     );
-    // return <LinkWrapper {...props} className={active?'active':'inactive'}></LinkWrapper>;
 };
 
 const LinkWrapper = styled.div`
     padding: 0.5rem 0;
     font-size: 45px;
-    color: gray;
     cursor: pointer;
     font-weight: bold;
     text-decoration: none;
-    & .active {
-        color: #2dc6ad;
-        filter: hue-rotate();
-        animation: hue-trans 7s infinite;
-    }
+    color: grey;
+    ${props => (props.theme.active ? `color: #2dc6ad;filter: hue-rotate();animation: hue-trans 7s infinite;` : ``)}
+
     &:hover {
         color: #2dc6ad;
         filter: hue-rotate();
         animation: hue-trans 7s infinite;
     }
 `;
-
-// const LinkWrapper = styled(Link)`
-//     padding: 0.5rem 0;
-//     font-size: 45px;
-//     color: gray;
-//     font-weight: bold;
-//     text-decoration: none;
-//     &:hover {
-//         color: #2dc6ad;
-//         filter: hue-rotate();
-//         animation: hue-trans 7s infinite;
-//     }
-// `;
 
 export default StyledLink;
