fd5729c0f1fc3d867df275f455d9c8f60c5156db
dimlights <qq174028@sina.com>
2020-02-02 21:19:26 +0800

    upload built file

---
+++ build/precache-manifest.2a379853072a277fb751cdda855d0b87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "879a4b88db81518b1da0e0f89c80a735",
+    "url": "/index.html"
+  },
+  {
+    "revision": "673ac2f8989a31f1d24f",
+    "url": "/static/css/main.538364a4.chunk.css"
+  },
+  {
+    "revision": "1088543049aebf6ea45a",
+    "url": "/static/js/2.3c2b8539.chunk.js"
+  },
+  {
+    "revision": "673ac2f8989a31f1d24f",
+    "url": "/static/js/main.57e6fff3.chunk.js"
+  },
+  {
+    "revision": "4721beea84fa606bc523",
+    "url": "/static/js/runtime-main.fd4df968.js"
+  },
+  {
+    "revision": "9b3bebf15bbc9a79b83562b60b82e896",
+    "url": "/static/media/iconfont.9b3bebf1.ttf"
+  }
+]);
