3d4380698e4d9ecb3dd7d96b742721c8ffee9cbe
Caper <deploy@travis-ci.org>
2020-02-13 16:20:35 +0000

    Deploy dimlights/isle to github.com/dimlights/isle.git:production

---
+++ build/precache-manifest.ac99e39cf88acdd485d204d0a3a51c1c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f06359532291cd275632e5495f1426d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d61b5d0c6313068e5cd6",
+    "url": "/static/css/main.538364a4.chunk.css"
+  },
+  {
+    "revision": "f4cdbc8dd739ec25a31a",
+    "url": "/static/js/3.febad044.chunk.js"
+  },
+  {
+    "revision": "302cd386b99d046ba682",
+    "url": "/static/js/4.1074d0bd.chunk.js"
+  },
+  {
+    "revision": "b51903dc3a5393bff993",
+    "url": "/static/js/StockChart.d737249d.chunk.js"
+  },
+  {
+    "revision": "d61b5d0c6313068e5cd6",
+    "url": "/static/js/main.72395336.chunk.js"
+  },
+  {
+    "revision": "8e2b2efa08431e923538",
+    "url": "/static/js/runtime-main.c3198f29.js"
+  },
+  {
+    "revision": "9b3bebf15bbc9a79b83562b60b82e896",
+    "url": "/static/media/iconfont.9b3bebf1.ttf"
+  }
+]);
