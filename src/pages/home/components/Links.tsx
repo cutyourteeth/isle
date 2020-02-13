0d3cdfa0497b62835239ed2cf78dc40c0bbc2e31
dimlights <qq174028@sina.com>
2020-02-13 21:18:12 +0800

    travis update

--- src/pages/home/components/Links.tsx
+++ src/pages/home/components/Links.tsx
@@ -49,6 +49,7 @@ export const Links = () => {
             {technicLinks.map(item => linkGenerator(item))}
             <Divide />
             {personalLinks.map(item => linkGenerator(item))}
+            <Divide />
         </LinksWrapper>
     );
 };
