156b31a892217fed3be5653e56ab526fb3f947f1
caperso <qq174028@sina.com>
2020-03-26 15:12:43 +0800

    update: remove stars and add solar

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
 import React from 'react';
-import styled from 'styled-components';
+import './assets/css/solar.css';
 import { Home } from './pages/home/Home';
 import { SiteRecord } from './pages/site-record/SiteRecord';
 
 const App: React.FC = () => {
     return (
         <>
-            <div id="stars2"></div>
-            <div id="stars3"></div>
-            <Pined>
-                <Home />
-                <SiteRecord />
-            </Pined>
+            <div className="solar-syst">
+                <div className="sun"></div>
+                <div className="mercury"></div>
+                <div className="venus"></div>
+                <div className="earth"></div>
+                <div className="mars"></div>
+                <div className="jupiter"></div>
+                <div className="saturn"></div>
+                <div className="uranus"></div>
+                <div className="neptune"></div>
+                <div className="pluto"></div>
+                <div className="asteroids-belt"></div>
+            </div>
+            <Home />
+            <SiteRecord />
         </>
     );
 };
-
-const Pined = styled.div`
-    position: fixed;
-    left: 0;
-    right: 0;
-    top: 0;
-    bottom: 0;
-    overflow-y:auto;
-`;
 
 export default App;
