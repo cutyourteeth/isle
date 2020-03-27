b580a7bd86f6a9102b3551092d98420a0e436316
Caper <deploy@travis-ci.org>
2020-03-27 06:34:14 +0000

    Deploy caperso/isle to github.com/caperso/isle.git:production

---
+++ build/precache-manifest.cc70ac198e554b1075f4048817861ec0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "271480f1ea7fa636962bcb490ddc288f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49cf6fc970e8a790281b",
+    "url": "/static/css/main.ad30a90f.chunk.css"
+  },
+  {
+    "revision": "76191103c359bddc69ac",
+    "url": "/static/js/3.2112ac7a.chunk.js"
+  },
+  {
+    "revision": "14f6c14ae342c5b392a4",
+    "url": "/static/js/4.b0aa36e1.chunk.js"
+  },
+  {
+    "revision": "32624fd9c4da307f01d5",
+    "url": "/static/js/StockChart.03a15f04.chunk.js"
+  },
+  {
+    "revision": "49cf6fc970e8a790281b",
+    "url": "/static/js/main.f590b3d7.chunk.js"
+  },
+  {
+    "revision": "da5efc43f73080f4b102",
+    "url": "/static/js/runtime-main.0a808edd.js"
+  },
+  {
+    "revision": "9b3bebf15bbc9a79b83562b60b82e896",
+    "url": "/static/media/iconfont.9b3bebf1.ttf"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.19d1048179db836025e2c0ee27270586.js"
+  "/precache-manifest.cc70ac198e554b1075f4048817861ec0.js"
 );
 
 self.addEventListener('message', (event) => {
